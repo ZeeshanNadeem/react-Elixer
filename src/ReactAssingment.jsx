301255dabaed5a9b6d1a92138e3418df9a4e9984
ZeeshanNadeem <y8785CjJr-Nq5d>
2022-05-26 20:11:35 +0500

    Update ReactAssingment.jsx

--- src/ReactAssingment.jsx
+++ src/ReactAssingment.jsx
@@ -36,6 +36,14 @@ const MyTable = () => {
     { value: ExtractData.ContractNumber, label: ExtractRead[4].key_, index: 4, draggerID: "ContractNumber" },
   ]);
 
+  const [rightTree_, setRightTree_] = React.useState([
+    { value: ExtractData.PlanID, label: ExtractRead[0].key_, index: 0, draggerID: "PlanID" },
+    { value: ExtractData.PlanName, label: ExtractRead[1].key_, index: 1, draggerID: "PlanName" },
+    { value: ExtractData.PlanType, label: ExtractRead[2].key_, index: 2, draggerID: "PlanType" },
+    { value: ExtractData.PlanYear, label: ExtractRead[3].key_, index: 3, draggerID: "PlanYear" },
+    { value: ExtractData.ContractNumber, label: ExtractRead[4].key_, index: 4, draggerID: "ContractNumber" },
+  ]);
+
   const [leftTree, setLeftTree] = React.useState([
     { value: plan.Plan1, label: planRead[0].key_, id: "322312323", draggerID: "Plan1" },
     { value: plan.PlanName1, label: planRead[1].key_, id: "3223ere23", draggerID: "PlanName1" },
@@ -48,11 +56,25 @@ const MyTable = () => {
 
     const items = Array.from(rightTree);
     const find = leftTree.filter(x => x.label === result.draggableId);
-    const newLeftTree = leftTree.filter(x => x.label != result.draggableId);
+    const newLeftTree = leftTree.filter(x => x.label !== result.draggableId);
+    console.log("destination:", result.destination.index)
+    let temp = {};
+    if (result.destination.index !== rightTree_.length - 1 && find[0] != undefined) {
+      temp = { id: find[0].id, value: find[0].value, label: find[0].label, draggerID: find[0].draggerID }
 
+      temp.label = `${find[0].label} mapped to ${rightTree_[result.destination.index].label}`
+      rightTree_.splice(result.destination.index, 0, temp);
+    }
     items.splice(result.destination.index, 0, find[0]);
     setLeftTree(newLeftTree);
     setRightTree(items);
+    if (Object.keys(temp).length > 0) {
+
+      setRightTree_(rightTree_)
+
+    }
+    else
+      setRightTree_(items)
   }
 
 
@@ -158,11 +180,11 @@ const MyTable = () => {
         </thead>
         <tbody>
 
-          {(leftTree.length >= rightTree.length ? leftTree : rightTree).map((_, index) => (
+          {(leftTree.length >= rightTree_.length ? leftTree : rightTree_).map((_, index) => (
 
             <tr>
               <td>{leftTree[index] != undefined && leftTree[index].label}</td>
-              <td>{rightTree[index] != undefined && rightTree[index].label}</td>
+              <td>{rightTree_[index] != undefined && rightTree_[index].label}</td>
             </tr>
           ))}
 
